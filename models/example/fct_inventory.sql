773b9289508521ccf02fd7ac17c101e870c47a7e
prajjvalmishra18@gmail.com <prajjvalmishra18@gmail.com>
2025-05-31 17:57:38 +0000

    fct_inventory

--- models/example/fct_inventory.sql
+++ models/example/fct_inventory.sql
@@ -2,6 +2,6 @@ SELECT
   id AS product_id,
   updated_at AS snapshot_date,
   'SKU001' AS sku,
-  stock_quantity AS afn_inventory_quantity,
-  stock_quantity AS afn_fulfillable_quantity
+  CAST(price AS INT64) AS afn_inventory_quantity,
+  CAST(price AS INT64) AS afn_fulfillable_quantity
 FROM `airbyte-demo-461419.airbyte_raw.products`
