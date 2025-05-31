282f857fe48852f9a0ff6ef60f753e11b7efb8c4
prajjvalmishra18@gmail.com <prajjvalmishra18@gmail.com>
2025-05-31 22:24:32 +0000

    update

--- models/example/mrt_kpi_daily.sql
+++ models/example/mrt_kpi_daily.sql
@@ -1,19 +1,18 @@
 WITH orders AS (
   SELECT DATE(order_date) AS date, sku, SUM(revenue) AS revenue
-  FROM {{ ref('fct_orders') }}
+  FROM `airbyte-demo-461419`.`dbt_pmishra`.`fct_orders`
   GROUP BY date, sku
 ),
 ads AS (
   SELECT DATE(ad_date) AS date, sku, SUM(ad_spend) AS ad_spend
-  FROM {{ ref('fct_ads') }}
+  FROM `airbyte-demo-461419`.`dbt_pmishra`.`fct_ads`
   GROUP BY date, sku
 ),
 inventory AS (
   SELECT DATE(snapshot_date) AS date, sku, MAX(afn_fulfillable_quantity) AS stock
-  FROM {{ ref('fct_inventory') }}
+  FROM `airbyte-demo-461419`.`dbt_pmishra`.`fct_inventory`
   GROUP BY date, sku
 )
-
 
 SELECT
   o.date,
@@ -22,7 +21,9 @@ SELECT
   a.ad_spend,
   ROUND(100 * a.ad_spend / NULLIF(o.revenue, 0), 2) AS tacos,
   i.stock,
-  ROUND(i.stock / NULLIF(SUM(o.revenue / 30), 0), 2) AS days_of_cover
+  ROUND(i.stock / NULLIF(o.revenue / 30, 0), 2) AS days_of_cover
 FROM orders o
 LEFT JOIN ads a ON o.date = a.date AND o.sku = a.sku
 LEFT JOIN inventory i ON o.date = i.date AND o.sku = i.sku
+GROUP BY
+  o.date, o.sku, o.revenue, a.ad_spend, i.stock
